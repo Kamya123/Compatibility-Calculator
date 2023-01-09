088a86556ef8bff4495c1f1b0ab2f495c2d76d5a
Kamya <kamyakumari140@gmail.com>
2023-01-09 19:32:06 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function calculateIt() {
     var yourName = document.getElementById('your-name').value;
     var crushName = document.getElementById('crush-name').value;
+    
     if (yourName != "" && crushName != "") {
         let percentage = Math.floor(Math.random() * 100);
         document.getElementById("result-msg").innerText = yourName + " and " + crushName + "'s chance of togetherness: ";
